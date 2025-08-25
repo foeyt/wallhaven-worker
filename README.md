cc772ab8ac0734244544aac49175d626653f358d
foeyt <foeyt522@gmail.com>
2025-08-25 17:56:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wallhaven-worker
+
+爬一张随机且 SFW 的 wallhaven 壁纸，定时运行。 
---
+++ src/wallpaper.py
@@ -0,0 +1,30 @@
+import requests
+import random
+from lxml import etree
+
+class WallHaven:
+    def __init__(self):
+        self.url = "https://wallhaven.cc/search?categories=110&purity=100&ratios=16x9&sorting=random&order=desc&seed=dRIyiE&page=" + str(random.randint(2, 8056))
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+
+    def get_page(self, url):
+        page = requests.get(url, headers=self.headers, timeout=30).content.decode("utf-8")
+        return page
+
+    def get_wallpaper(self):
+        html = self.get_page(self.url)
+        wallpaper_list = etree.HTML(html).xpath("//figure/a/@href")
+        return wallpaper_list
+    
+    def download_image(self, wallpaper):
+        html = etree.HTML(self.get_page(wallpaper))
+        names = html.xpath("//div[@class=\"scrollbox\"]//img/@src")
+        with open("wallpaper.png", "wb") as f:
+            f.write(requests.get(url=names[0], headers=self.headers, timeout=30).content)
+
+wallhaven = WallHaven()
+wallhaven.download_image(wallhaven.get_wallpaper()[random.randint(0, len(wallhaven.get_wallpaper()) - 1)])
+
+
