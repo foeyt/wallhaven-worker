febd18b664c12cab68f14b50aaa7c4f0f10ec4d5
foeyt <foeyt522@gmail.com>
2025-08-25 18:27:52 +0800

    fix

--- src/wallpaper.py
+++ src/wallpaper.py
@@ -4,22 +4,25 @@ from lxml import etree
 
 class WallHaven:
     def __init__(self):
-        self.url = "https://wallhaven.cc/search?categories=110&purity=100&ratios=16x9&sorting=random&order=desc&seed=dRIyiE&page=" + str(random.randint(2, 8056))
+        self.url = "https://wallhaven.cc/search?categories=110&purity=100&atleast=1920x1080&sorting=random&order=desc&seed=HmUG9N&page=" + str(random.randint(2, 8056))
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
         }
 
     def get_page(self, url):
-        page = requests.get(url, headers=self.headers, timeout=30).content.decode("utf-8")
+        page = requests.get(url, headers=self.headers, timeout=10000).content.decode("utf-8")
         return page
 
     def get_wallpaper(self):
         html = self.get_page(self.url)
         wallpaper_list = etree.HTML(html).xpath("//figure/a/@href")
+        if len(wallpaper_list) == 0:
+            print("FAILED, RETRYING...")
+            self.get_wallpaper()
         return wallpaper_list
     
     def download_image(self, wallpaper):
-        print("GETTING..." + wallpaper)
+        print("GETTING... " + wallpaper)
         html = etree.HTML(self.get_page(wallpaper))
         names = html.xpath("//div[@class=\"scrollbox\"]//img/@src")
         with open("wallpaper.png", "wb") as f:
